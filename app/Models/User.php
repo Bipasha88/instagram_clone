baa93ae8279ffa0a7ce5b10b3409228b384a21df
Bipasha88 <dolasaha88@gmail.com>
2021-02-02 00:26:12 -0800

    follow api created

--- app/Models/User.php
+++ app/Models/User.php
@@ -58,4 +58,7 @@ class User extends Authenticatable
     public function followers(){
         return $this->hasMany(Follow::class);
     }
+    public function follow(){
+        return $this->hasMany(Follow::class);
+    }
 }
